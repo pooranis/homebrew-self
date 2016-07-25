caaa2afc92bb376d9467b63c8da619c064e0d5e7
pooranis <pooranis@users.noreply.github.com>
2016-07-25 12:58:14 -0400

    Update version.

--- kronatools.rb
+++ kronatools.rb
@@ -1,7 +1,7 @@
 class Kronatools < Formula
   desc "Krona Tools is a set of scripts to create Krona charts from several Bioinformatics tools as well as from text and XML files."
   homepage "https://github.com/marbl/Krona/wiki"
-  url "https://github.com/marbl/Krona/releases/download/v2.6.1/KronaTools-2.6.1.tar"
+  url "https://github.com/marbl/Krona/releases/download/2.7/KronaTools-2.7.tar"
   sha256 "9f4787a240366bc156e3c0ed14667462f0eb0e5f4e6961c9d9e7f3c58dab45ba"
 
   skip_clean "taxonomy"
