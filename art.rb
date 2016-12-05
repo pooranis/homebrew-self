b215a4d29e0ca22ab95f12e9fd791643e80d13fa
pooranis <pooranis@users.noreply.github.com>
2016-12-05 07:39:15 -0500

    add art read simulator formula

---
+++ art.rb
@@ -0,0 +1,34 @@
+class Art < Formula
+  desc "Simulation tools to generate synthetic NGS reads"
+  homepage "http://www.niehs.nih.gov/research/resources/software/biostatistics/art/index.cfm"
+  # doi "10.1093/bioinformatics/btr708"
+  # tag "bioinformatics"
+
+  if OS.mac?
+    url "https://www.niehs.nih.gov/research/resources/assets/docs/artsrcmountrainier20160605macostgz.tgz"
+    version "20160605"
+    sha256 "1c467c374ec17b1c2c815f4c24746bece878876faaf659c2541f280fe7ba85f7"
+
+  revision 1
+
+  depends_on "gsl"
+
+  def install
+    ENV.append "CPPFLAGS", "-I#{Formula["gsl"].opt_include}"
+    ENV.append "LDFLAGS",  "-L#{Formula["gsl"].opt_lib}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # remove the bundled binaries so they get re-made against our libraries
+    system "make", "clean"
+    system "make", "install"
+    doc.install %w[AUTHORS COPYING ChangeLog NEWS README art_454_README art_SOLiD_README art_illumina_README]
+    pkgshare.install %w[examples 454_profiles Illumina_profiles SOLiD_profiles]
+  end
+
+  test do
+    system "#{bin}/art_illumina | grep 'MiSeq'"
+    system "#{bin}/art_SOLiD | grep 'F3-F5'"
+    system "#{bin}/art_454 | grep 'FLX'"
+  end
+end
