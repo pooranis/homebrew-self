5f2592a7b8591c610193def91621468e115858fb
pooranis <pooranis@users.noreply.github.com>
2019-09-19 13:01:55 -0400

    add aliview cask

---
+++ Casks/aliview.rb
@@ -0,0 +1,11 @@
+cask 'aliview' do
+  version '1.26'
+  sha256 '38851e64d05e2f5a4c23675c4258c8c7c49cba52efe06c5bf8c2989f33f63b4e'
+
+  url "https://ormbunkar.se/aliview/downloads/mac/AliView-#{version}-app.zip"
+  appcast 'https://github.com/AliView/AliView/releases.atom'
+  name 'AliView'
+  homepage 'https://ormbunkar.se/aliview/'
+
+  app "AliView-#{version}/AliView.app"
+end
