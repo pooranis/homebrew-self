d5f31b6a304f9d7bc03977c72d0a9d261650a288
pooranis <pooranis@users.noreply.github.com>
2020-07-20 12:47:04 -0400

    update ugene

--- Casks/ugene.rb
+++ Casks/ugene.rb
@@ -1,8 +1,7 @@
 cask 'ugene' do
-  version '1.32.0'
-  sha256 '5d80dd8cbbe4a7cd123a020850845b82180f2d77882a146b2047d2ac9b021bae'
-
-  url "http://ugene.unipro.ru/downloads/ugene-#{version}-mac-x86-64.dmg"
+  version '35.0'
+  sha256 '9ef6d40987898a58ec8c9c97ac526178b0591969c72d9422c336cc444ad733ad'
+  url "http://ugene.unipro.ru/downloads/packages/#{version}/ugene-#{version}-mac-x86-64.dmg"
   appcast 'http://ugene.net/downloads/ugene_get_latest_mac_x86_64.html'
   name 'UGENE'
   homepage 'http://ugene.net/'
