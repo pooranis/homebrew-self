271217f554683340d5bad57f4ea01c8d46dbd2c7
pooranis <pooranis@users.noreply.github.com>
2023-10-22 16:06:15 -0400

    version bump to cpdf 2.6.1

--- cpdf.rb
+++ cpdf.rb
@@ -1,10 +1,9 @@
 class Cpdf < Formula
-  desc "The Coherent PDF Command Line Tools allow you to manipulate existing PDF files in a variety of ways.  Free for non-commercial, private, personal use.  See homepage for more information."
+  desc "The Coherent PDF Command Line Tools allow you to manipulate existing PDF files in a variety of ways.  Free for non-commercial, private, personal use.  See homepage for more information.  See https://github.com/coherentgraphics/cpdf-binaries/blob/master/LICENSE for licensing"
   homepage "https://community.coherentpdf.com/"
-  version "2.5.2"
-  # url "https://github.com/coherentgraphics/cpdf-binaries/archive/master.zip"
-  url "https://github.com/coherentgraphics/cpdf-binaries/archive/v#{version}.zip"
-  sha256 "147414eba50313ad459dd17f884d922355e3abc4efc67c79177f28f1a0c9a0d5"
+  version "2.6.1"
+  url "https://github.com/coherentgraphics/cpdf-binaries/archive/refs/tags/v#{version}.zip"
+  sha256 "eabec23dcc4f64cfb4e003e3c85cd6460805982b73f22e316639e3a2143d9444"
   license :cannot_represent
 
 
